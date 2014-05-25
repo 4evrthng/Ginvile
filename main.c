d6887419dc8c47c8ea5a16eba41d2ab9d6e9e8e8
4evrthng <4evrthng@gmail.com>
2014-05-25 20:01:06 +0300

    done

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 int main(int argc, char *argv[]){
 FILE *fp1;
 FILE *fout;
-char oneword[100];
-char c;
+char *oneword;
+char *c;
 int t;
 bool quote;
 bool comment;
@@ -47,242 +47,121 @@ char *blah[] = {"small", ".stack", ".data", "db", ".code", "@data"} ;
 	fprintf(fout, "<body> \n");
 	fprintf(fout, "<pre>\n");
 
-
- /*   while (c != EOF){
-        c = fscanf(fp1,"%s",oneword);
-
-        if ( (   (strcasecmp(oneword,"mov") == 0) //instructions
-            || (strcasecmp(oneword,"int") == 0)
-            || (strcasecmp(oneword,"push") == 0)
-            || (strcasecmp(oneword,"ret") == 0)
-            || (strcasecmp(oneword,"pop") == 0)
-            || (strcasecmp(oneword,"call") == 0)
-            || (strcasecmp(oneword,"lea") == 0)
-            || (strcasecmp(oneword,"cmp") == 0)
-            || (strcasecmp(oneword,"add") == 0)
-            || (strcasecmp(oneword,"jmp") == 0)
-            || (strcasecmp(oneword,"sub") == 0)
-            || (strcasecmp(oneword,"shr") == 0)
-            || (strcasecmp(oneword,"inc") == 0)
-            || (strcasecmp(oneword,"dec") == 0)
-            || (strcasecmp(oneword,"imul") == 0)
-            || (strcasecmp(oneword,"shl") == 0)
-            || (strcasecmp(oneword,"neg") == 0)
-            || (strcasecmp(oneword,"not") == 0)
-            || (strcasecmp(oneword,"div") == 0)
-            || (strcasecmp(oneword,"and") == 0)
-            || (strcasecmp(oneword,"xor") == 0)
-            || (strcasecmp(oneword,"or") == 0)
-            || (strcasecmp(oneword,"JE") == 0)
-            || (strcasecmp(oneword,"JLE") == 0)
-            || (strcasecmp(oneword,"JMP") == 0)
-            || (strcasecmp(oneword,"loop") == 0)
-            || (oneword[0] == '\n')))
+    char line [1000];
+    int i, number;
+    char *ch;
+    while((fgets(line, sizeof(line), fp1)) != NULL)
+    {
+        oneword = strtok(line, " ");
+        comment = false;
+        while (oneword != NULL)
         {
-            if (comment)
-                {
-                    fprintf(fout, "<font color = 'orange'> %s </font>", oneword);
-                }
-            else
+            if(oneword[0] == 59 || comment)//kabliataskis
             {
-                if(quote)
-                 {
-                    fprintf(fout, "<font color = 'purple'> %s </font>", oneword);
-                 }
-                else
-                {
-                    fprintf(fout,"<br>");
-                    fprintf(fout,"<font color = 'blue'> %s </font>" , oneword); //<font color ="blue">',Zd[i],'</font>'
-                    }
-             }
+                fprintf(fout, "<font color = 'orange'> %s </font>", oneword);
+                comment = true;
+            }
+            else if((oneword[0] == 39)||quote)
+            {
+                fprintf(fout, "<font color = 'purple'> %s </font>", oneword);
+
+                    c = oneword+1;
+                    //fprintf(fout,  "<font color = 'pink'> %i </font>" , strchr(c,'\''));
+                    if(strchr(c,'\'')> 0)quote = false;
+                    else quote = true;
+            }
+            else if (   (strcasecmp(oneword,"mov") == 0) //instructions
+                || (strcasecmp(oneword,"int") == 0)
+                || (strcasecmp(oneword,"push") == 0)
+                || (strcasecmp(oneword,"ret") == 0)
+                || (strcasecmp(oneword,"pop") == 0)
+                || (strcasecmp(oneword,"call") == 0)
+                || (strcasecmp(oneword,"lea") == 0)
+                || (strcasecmp(oneword,"cmp") == 0)
+                || (strcasecmp(oneword,"add") == 0)
+                || (strcasecmp(oneword,"jmp") == 0)
+                || (strcasecmp(oneword,"sub") == 0)
+                || (strcasecmp(oneword,"shr") == 0)
+                || (strcasecmp(oneword,"inc") == 0)
+                || (strcasecmp(oneword,"dec") == 0)
+                || (strcasecmp(oneword,"imul") == 0)
+                || (strcasecmp(oneword,"shl") == 0)
+                || (strcasecmp(oneword,"neg") == 0)
+                || (strcasecmp(oneword,"not") == 0)
+                || (strcasecmp(oneword,"div") == 0)
+                || (strcasecmp(oneword,"and") == 0)
+                || (strcasecmp(oneword,"xor") == 0)
+                || (strcasecmp(oneword,"or") == 0)
+                || (strcasecmp(oneword,"JE") == 0)
+                || (strcasecmp(oneword,"JLE") == 0)
+                || (strcasecmp(oneword,"JMP") == 0)
+                || (strcasecmp(oneword,"loop") == 0))
+        {
+            fprintf(fout,"<font color = 'blue'> %s </font>" , oneword); //<font color ="blue">',Zd[i],'</font>'
         }
         else if (  (strcasecmp(oneword,"small") == 0)
                 || (strcasecmp(oneword,".stack") == 0)
-                || (strcasecmp(oneword,".data") == 0)
+                || (strcasecmp(oneword,".data\n") == 0)
                 || (strcasecmp(oneword,"db") == 0)
-                || (strcasecmp(oneword,".code") == 0)
-                || (strcasecmp(oneword,"@data") == 0)
-                || (oneword[0] == '\n')
-
-            )
+                || (strcasecmp(oneword,".code\n") == 0)
+                || (strcasecmp(oneword,"@data") == 0))
             {
-                if (comment)
-                {
-                    fprintf(fout, "<font color = 'orange'> %s </font>", oneword);
-                }
-                else
-                {
-                    if(quote)
-                    {
-                        fprintf(fout, "<font color = 'purple'> %s </font>", oneword);
-                    }
-                    else
-                    {
-                            fprintf(fout,"<br>");
-                            fprintf(fout,"<font color = 'cyan'> %s </font>" , oneword);
-                    }
-
-                 }
-             }
-        else if( (strcasecmp(oneword,"ax,") == 0)
-                  || (strcasecmp(oneword,"ah,") == 0)
-                  || (strcasecmp(oneword,"dx,") == 0)
-                  || (strcasecmp(oneword,"ds,") == 0)
-                  || (strcasecmp(oneword,"ax") == 0)
-                  || (strcasecmp(oneword,"bx") == 0)
-                  || (strcasecmp(oneword,"cx") == 0)
-                  || (strcasecmp(oneword,"dx") == 0)
-                  || (strcasecmp(oneword,"al") == 0)
-                  || (strcasecmp(oneword,"bl") == 0)
-                  || (strcasecmp(oneword,"cl") == 0)
-                  || (strcasecmp(oneword,"dl") == 0)
-                  || (strcasecmp(oneword,"ah") == 0)
-                  || (strcasecmp(oneword,"bh") == 0)
-                  || (strcasecmp(oneword,"ch") == 0)
-                  || (strcasecmp(oneword,"dh") == 0)
-                  || (strcasecmp(oneword,"sp") == 0)
-                  || (strcasecmp(oneword,"bp") == 0)
-                  || (strcasecmp(oneword,"si") == 0)
-                  || (strcasecmp(oneword,"di") == 0)
-                  || (strcasecmp(oneword,"bp,") == 0)
-                  || (strcasecmp(oneword,"si") == 0)
-                  || (strcasecmp(oneword,"ax,") == 0)
-                  || (strcasecmp(oneword,"bx,") == 0)
-                  || (strcasecmp(oneword,"cx,") == 0)
-                  || (strcasecmp(oneword,"dx,") == 0)
-                  || (strcasecmp(oneword,"al,") == 0)
-                  || (strcasecmp(oneword,"bl,") == 0)
-                  || (strcasecmp(oneword,"cl,") == 0)
-                  || (strcasecmp(oneword,"dl,") == 0)
-                  || (strcasecmp(oneword,"ah,") == 0)
-                  || (strcasecmp(oneword,"bh,") == 0)
-                  || (strcasecmp(oneword,"ch,") == 0)
-                  || (strcasecmp(oneword,"dh,") == 0)
-                  || (strcasecmp(oneword,"sp,") == 0)
-                  || (strcasecmp(oneword,"bp,") == 0)
-                  || (strcasecmp(oneword,"si,") == 0)
-                  || (strcasecmp(oneword,"di,") == 0)
-                  || (strcasecmp(oneword,"si,") == 0)
-                )
-                {
-                    if (comment)
-                    {
-                        fprintf(fout, "<font color = 'orange'> %s </font>", oneword);
-
-                    }
-                    else
-                    {
-                        if(quote)
-                        {
-                          fprintf(fout, "<font color = 'purple'> %s </font>", oneword);
-                        }
-
-                       else
-                       {
-                            fprintf(fout, "<font color = 'green'> %s </font>", oneword);
-
-                       }
-                    }
-
-                }
-
-        else
-        {
-            if(oneword[0] == 59)//kabliataskis
-            {
-               // fprintf(fout,"<br>");
-                fprintf(fout, "<font color = 'orange'> %s </font>", oneword);
-                comment = true;
-                if (strcasecmp(oneword,"\n"))
-                    {
-                        fprintf(fout, "<br>");
-                        comment = false;
-                    }
+                fprintf(fout,"<font color = 'cyan'> %s </font>" , oneword);
             }
-            else if (comment)
-            {
-                //fprintf(fout, "<br>");
-                fprintf(fout, "<font color = 'orange'> %s </font>", oneword);
-                  if (!strcasecmp(oneword,"\n"))
-                    {
-                        fprintf(fout, "<br>");
-                        comment = false;
-                    }
-            }
-            else
-            {
-                if((oneword[0] == 39) &&  !quote)
-                {
-                    fprintf(fout, "<font color = 'purple'> %s </font>", oneword);
-                    quote = true;
-                    if (!strcasecmp(oneword,"\n"))
-                    {
-                        fprintf(fout, "kabliataskio br");
-                        comment = false;
-                    }
-                }
-                if(quote)
-                {
-                    if((oneword[strlen(oneword) - 1] == 39) || (oneword[strlen(oneword) - 2] == 39))
-                    {
-                        quote = false;
-                        //fprintf(fout,"<br>");
-                    }
-                    if(oneword[0] != 39)
-                    {
-                        fprintf(fout, "<font color = 'purple'> %s </font>", oneword);
-                        if (!strcasecmp(oneword,"\n"))
-                        {
-                            fprintf(fout, "yolo");
-                            comment = false;
-                        }
-                    }
-                }
-                else if ((oneword[0] >= 48) && (oneword[0] <= 57))
-                {
-
-                    fprintf(fout, "<font color = 'red'> %s </font>", oneword);
-                     if (!strcasecmp(oneword,"\n"))
-                    {
-                        fprintf(fout, "<br>");
-                        comment = false;
-                    }
-                }
-
-                else
-                {
-                 //   fprintf(fout,"<br>");
-                    fprintf(fout,"%s ",oneword);
-                    //fprintf(fout,"%i" , atol(oneword));
-                      if (!strcasecmp(oneword,"\n"))
-                    {
-                        fprintf(fout, "<br>");
-                        comment = false;
-                    }
-                }
-
-             }
-            //first or last
+         else if ( ( strcasecmp(oneword,"ax,") == 0)
+          || (strcasecmp(oneword,"ah,") == 0)
+          || (strcasecmp(oneword,"dx,") == 0)
+          || (strcasecmp(oneword,"ds,") == 0)
+          || (strcasecmp(oneword,"ax") == 0)
+          || (strcasecmp(oneword,"bx") == 0)
+          || (strcasecmp(oneword,"cx") == 0)
+          || (strcasecmp(oneword,"dx") == 0)
+          || (strcasecmp(oneword,"al") == 0)
+          || (strcasecmp(oneword,"bl") == 0)
+          || (strcasecmp(oneword,"cl") == 0)
+          || (strcasecmp(oneword,"dl") == 0)
+          || (strcasecmp(oneword,"ah") == 0)
+          || (strcasecmp(oneword,"bh") == 0)
+          || (strcasecmp(oneword,"ch") == 0)
+          || (strcasecmp(oneword,"dh") == 0)
+          || (strcasecmp(oneword,"sp") == 0)
+          || (strcasecmp(oneword,"bp") == 0)
+          || (strcasecmp(oneword,"si") == 0)
+          || (strcasecmp(oneword,"di") == 0)
+          || (strcasecmp(oneword,"bp,") == 0)
+          || (strcasecmp(oneword,"si") == 0)
+          || (strcasecmp(oneword,"ax,") == 0)
+          || (strcasecmp(oneword,"bx,") == 0)
+          || (strcasecmp(oneword,"cx,") == 0)
+          || (strcasecmp(oneword,"dx,") == 0)
+          || (strcasecmp(oneword,"al,") == 0)
+          || (strcasecmp(oneword,"bl,") == 0)
+          || (strcasecmp(oneword,"cl,") == 0)
+          || (strcasecmp(oneword,"dl,") == 0)
+          || (strcasecmp(oneword,"ah,") == 0)
+          || (strcasecmp(oneword,"bh,") == 0)
+          || (strcasecmp(oneword,"ch,") == 0)
+          || (strcasecmp(oneword,"dh,") == 0)
+          || (strcasecmp(oneword,"sp,") == 0)
+          || (strcasecmp(oneword,"bp,") == 0)
+          || (strcasecmp(oneword,"si,") == 0)
+          || (strcasecmp(oneword,"di,") == 0)
+          || (strcasecmp(oneword,"si,") == 0))
+         {
+            fprintf(fout,"<font color = 'green'> %s </font>" , oneword);
+         }
+        else if ((oneword[0] >= 48) && (oneword[0] <= 57)) fprintf(fout, "<font color = 'red'> %s </font>", oneword);
+        else fprintf(fout, "%s ", oneword);
+        //fprintf(fout, "%s",
+        oneword = strtok(NULL, " ");
         }
-            //}
-    }    /repeat until EOF          */
+          //fprintf(fout, "%s", line);
+        }
 
-  /*  char * line = NULL;
-    size_t len = 0;
-    int i ;
-    size_t read;
-    while((read = getline(&line, & len, fp1)) != -1){
-        c = line[i];
-        if (isspace(c)) c = ' ';
-        //printf(" help...\n");
-        putchar(c);
-        //fprintf(fout, "%s", line);
-    }
-
-
+/*
     char *pch;
     pch = strtok (line,  ", ");
-    int i = 0;*/
+    int i = 0;
     int i = 0;
     char* line;
     while((fgets(line, sizeof(line), fp1) != NULL))
@@ -292,7 +171,7 @@ char *blah[] = {"small", ".stack", ".data", "db", ".code", "@data"} ;
         printf("%c", c);
     }
 
-    fprintf(fout, "</pre> \n");
+    fprintf(fout, "</pre> \n");*/
     fprintf(fout,"</body></html>");
     fclose(fp1);
 
